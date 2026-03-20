44a7e06a3a1cef82f3704c0330662ee49ed95e27
katayano <idnlnroocwetn22@gmail.com>
2026-03-21 01:16:47 +0900

    チュートリアル画面作成

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnTogetherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Tutorial(
+                        title = stringResource(R.string.tutorial_title),
+                        overview = stringResource(R.string.tutorial_overview),
+                        detail = stringResource(R.string.tutorial_detail),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Tutorial(title: String, overview: String, detail: String, modifier: Modifier = Modifier) {
+    val headerImage = painterResource(id = R.drawable.bg_compose_background)
+    Column (modifier = modifier) {
+        Image(
+            painter = headerImage,
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = overview,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+        )
+        Text(
+            text = detail,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        Tutorial(
+            stringResource(R.string.tutorial_title),
+            stringResource(R.string.tutorial_overview),
+            stringResource(R.string.tutorial_detail)
+            )
     }
 }
